292cddb2929985aea76371d18d81c6a9c9593b73
cuiyijie <cyj976655@gmail.com>
2021-01-17 16:58:42 +0800

    a

--- meizi/settings.py
+++ meizi/settings.py
@@ -15,7 +15,8 @@ SPIDER_MODULES = ['meizi.spiders']
 NEWSPIDER_MODULE = 'meizi.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 ' \
+             'Safari/537.36 '
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
